779a96d9b6bb022b7834bad9ba3523e22f933b0b
mucopivoine <pivotmurerwa1@gmail.com>
2024-03-16 13:07:43 +0200

    navbar task project

--- bar.js
+++ bar.js
@@ -1,5 +1,6 @@
-const bar = document.querySelector(".fa-bars");
-const menu= document.querySelector(".menu");
-bar.addEventListener("click", () =>{
-    menu.classList.toggle("show-menu");
-});
+const navEl = document.querySelector('header .navigation ul');
+const menuBtn =document.querySelector('.menu-btn');
+menuBtn.addEventListener('click',() =>{
+navEl.classList.toggle("show-menu");
+
+})
